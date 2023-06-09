ac7b5de0068ea924dde3e9b1bff46d665165a3ab
Molayasa <paulinaolaya1@gmail.com>
2023-06-08 19:20:56 -0500

    Create endpoint to POST a new song

--- src/controllers/postSong.controller.js
+++ src/controllers/postSong.controller.js
@@ -1,5 +1,24 @@
+const Song = require('../models/Song');
+
 const postSong = async (req, res) => {
-  res.json({ message: 'Not implemented' });
+  const { title, length, artistId, albumId, genreId } = req.body;
+
+  try {
+    const song = await Song.create({
+      title,
+      length,
+      artist_id: artistId,
+      album_id: albumId,
+      genre_id: genreId,
+    });
+
+    res.status(201).json({ data: song, message: 'Song created' });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "There's a problem with the server. Try again later" });
+  }
 };
 
 module.exports = { postSong };
