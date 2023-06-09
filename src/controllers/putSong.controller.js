dce10c903c53fcfae629ea4440ad092fdb748a52
Molayasa <paulinaolaya1@gmail.com>
2023-06-08 19:31:59 -0500

    Create endpoint to PUT an existing song

--- src/controllers/putSong.controller.js
+++ src/controllers/putSong.controller.js
@@ -1,5 +1,38 @@
-const putSong = (req, res) => {
-  res.json({ message: 'Not implemented' });
+const Song = require('../models/Song');
+
+const putSong = async (req, res) => {
+  try {
+    const { songId } = req.params;
+
+    const existingSong = await Song.findByPk(songId, {
+      attributes: ['title', 'length', 'album_id', 'artist_id', 'genre_id'],
+      raw: true,
+    });
+
+    if (!existingSong) {
+      return res.status(404).json({ message: 'Song not found' });
+    }
+
+    const { title, length, album_id, artist_id, genre_id } = req.body;
+
+    const updatedSong = {
+      ...existingSong,
+      ...req.body,
+    };
+
+    await Song.update(updatedSong, {
+      where: { id: songId },
+    });
+
+    res
+      .status(200)
+      .json({ data: updatedSong, message: 'Song updated successfully' });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "There's a problem with the server. Try again later" });
+  }
 };
 
 module.exports = { putSong };
